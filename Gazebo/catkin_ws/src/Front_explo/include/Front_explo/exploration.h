6966acf05ff02094b4517d2c0145017835af2695
Vinh Trung <trung_thai@live.dk>
2022-03-31 09:36:50 +0200

    cleanup

--- Gazebo/catkin_ws/src/Front_explo/include/Front_explo/exploration.h
+++
@@ -1,7 +0,0 @@
-#include <costmap_2d/costmap_2d_ros.h>
-#include <costmap_2d/costmap_2d.h>
-
-#include <boost/foreach.hpp>
-#include <string>
-#include <vector>
-#include <tf/transform_listener.h>
--- Gazebo/catkin_ws/src/Front_explo/src/exploration.cpp
+++
@@ -1,8 +0,0 @@
-#include <exploration.h>
-#include <pluginlib/class_list_macros.h>
-#include <frontier_exploration/frontier_plugin.h>
-
-#include <exploration_server/base_plugin.h>
-#include <visualization_msgs/Marker.h>
-#include <list>
-#include <vector>
