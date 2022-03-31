e09f4bba9294119a9eaa467be24dd3fe8527cb2f
Liping S <liping@eng.au.dk>
2022-03-31 13:05:06 +0000

    Now add mads mappe

---
+++ Gazebo/octomap_ros-melodic-devel/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/root/AutoSSAR/Gazebo/octomap_ros-melodic-devel/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/root/AutoSSAR/Gazebo/octomap_ros-melodic-devel/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/root/AutoSSAR/Gazebo/octomap_ros-melodic-devel:/root/AutoSSAR/Gazebo/catkin_ws/src:/root/catkin_ws/src:/opt/ros/melodic/share'
