5c814d89d5f04c33f2f0904efb200de1cd7a9742
Liping S <liping@eng.au.dk>
2022-04-07 13:41:37 +0000

    install stuff

--- Gazebo/catkin_ws/src/marble_mapping/README.md
+++
@@ -1,6 +0,0 @@
-marble_mapping
-===============
-
-Octomap mapping with merging package for MARBLE DARPA Subterranean Challenge Team.  Derived from octomap_server located at:
-
-https://github.com/OctoMap/octomap_mapping
--- Gazebo/catkin_ws/src/marble_mapping/scripts/sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git remote add upstream https://github.com/dan-riley/marble_mapping
-git fetch upstream
-git checkout master
-git reset --hard upstream/master
-# git rebase upstream/master
-git push origin master --force
