007595a0a3eaf5c7b0042b6c1fb0dc470521c5e8
Vinh Trung <trung_thai@live.dk>
2022-04-18 14:53:42 +0200

    callbacks and merging

--- Gazebo/catkin_ws/src/autoSSAR/src/mapmerge.cpp
+++ Gazebo/catkin_ws/src/autoSSAR/src/mapmerge.cpp
@@ -22,82 +22,47 @@
 
 using namespace std;
 
-sensor_msgs::PointCloud2 own_globalMap_pcd;
+pcl::PointCloud<pcl::PointXYZ> own_globalMap_pcd;
+pcl::PointCloud<pcl::PointXYZ> local_map_pcd;
 sensor_msgs::PointCloud2 rcv_globalMap_pcd;
-sensor_msgs::PointCloud2 localMap_pcd;
-pcl::PointCloud<pcl::PointXYZ> cloudMap;
+sensor_msgs::PointCloud2 globalMap_pcd;
+
+
+void getMapCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+    sensor_msgs::PointCloud2 localMap_pcd = *msg;
+    pcl::PointCloud<pcl::PointXYZ> cloudMap;
+    pcl::fromROSMsg(localMap_pcd, cloudMap);
+    own_globalMap_pcd += cloudMap;
+    cloudMap.clear();
+}
 
 
 void getLocalMapCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 {
-    localMap_pcd = *msg;
+    sensor_msgs::PointCloud2 localMap_pcd = *msg;
+    pcl::fromROSMsg(localMap_pcd, cloudMap);
+    local_map_pcd += cloudMap;
     cloudMap.clear();
-    pcl::fromROSMsg(rcv_globalMap_pcd, cloudMap);
 }
-
-void getGlobalMapCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
-{
-    rcv_globalMap_pcd = *msg;
-    cloudMap.clear();
-    pcl::fromROSMsg(own_globalMap_pcd, cloudMap);
-}
-
-void mergeMaps(sensor_msgs::PointCloud2 own, sensor_msgs::PointCloud2 recv)
-{
-    if(own != 0){
-        pcl::PointCloud<pcl::PointXYZ> cloudOwn;
-        pcl::PointCloud<pcl::PointXYZ> cloudRecv;
-        pcl::PointCloud<pcl::PointXYZ> cloudMerged;
-
-        cloudOwn.clear();
-        cloudRecv.clear();
-        cloudMerged.clear();
-
-        pcl::fromROSMsg(own, cloudOwn);
-        pcl::fromROSMsg(recv, cloudRecv);
-
-        cloudMerged = cloudOwn + cloudRecv;
-        own_globalMap_pcd = cloudMerged;
-        rcv_globalMap_pcd.clear();
-    }
-    else(
-        own_globalMap_pcd = recv;
-    )
-}
-
-
-
 
 int main (int argc, char** argv){
     ros::init(argc, argv, "map_merger");
     ros::NodeHandle nh;
     ros::NodeHandle nh_private("~");
 
-    //Local map subscriber
-    ros::Subscriber map_sub = nh.subscribe("/pcl_render_node/local_map", 1, getLocalMapCallback);
-    ros::Subscriber map_sub2 = nh.subscribe("/map_generator/global_cloud", 1, getGlobalMapCallback);
-    //merge local received map with own global map
-    if(localMap_pcd.data.size() == 0){
-        ROS_WARN("No map received");
-    }
-    else{
-        ROS_WARN("Map received");
-        mergeMaps(own_globalMap_pcd, localMap_pcd);
-    }
-
-    //Glocal map subscriber
-
-    // ROS_WARN("Subscribed");
-    if(rcv_globalMap_pcd.data.size() == 0){
-        ROS_WARN("No map received");
-    }
-    else{
-        ROS_WARN("Map received");
-        mergeMaps(own_globalMap_pcd, rcv_globalMap_pcd);
-    }
-
-
+    //Pub Subs
+    ros::Subscriber map_sub = nh.subscribe("/pcl_render_node/local_map", 1, getMapCallback);
+    ros::Subscriber map_sub2 = nh.subscribe("/map_generator/global_cloud", 1, getMapCallback);
     ros::Publisher map_pub = nh.advertise<sensor_msgs::PointCloud2>("/map_merged", 1);
+    //merge local received map with own global map
+
+    //Publish merged map
+    pcl::toROSMsg(own_globalMap_pcd, globalMap_pcd);
+    
+
+    map_pub.publish(globalMap_pcd);
+    
     ros::Rate loop_rate(10);
 
     return 0;
