838be95cee379c8479a1f1709b4038e677883e36
Vinh Trung <trung_thai@live.dk>
2022-04-22 12:32:01 +0200

    MapMerge Funktion

--- catkin_ws/src/autoSSAR/src/mapmerge.cpp
+++ catkin_ws/src/autoSSAR/src/mapmerge.cpp
@@ -1,14 +1,12 @@
 // #include "include/mapmerge.h"
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
-
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 // #include <pcl/search/kdtree.h>
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <iostream>
-
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Vector3.h>
 #include <math.h>
@@ -16,12 +14,11 @@
 #include <ros/console.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <Eigen/Eigen>
-#include <random>
 #include <octomap_msgs/Octomap.h>
 #include <octomap_msgs/conversions.h>
 #include <octomap/octomap.h>
-#include <map_merge_3d>
+#include <pcl/registration/icp.h>
+#include <pcl/filters/filter.h>
 
 
 using namespace std;
@@ -62,12 +59,30 @@ void getLocalMapCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 void mergeMaps(pcl::PointCloud<pcl::PointXYZ>& cloudMap)
 {
     ROS_WARN("Merging maps");
-    // transform received
 
-    // find closest point in own_globalMap_pcd
-    // if distance < threshold, add to own_globalMap_pcd
-    // else add to own_globalMap_pcd
+    //Remove NAN points
+    std::vector<int> indices;
+    pcl::removeNaNFromPointCloud(cloudMap, cloudMap, indices);
 
+    pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+    icp.setInputSource(cloudMap);
+    icp.setInputTarget(own_globalMap_pcd);
+    icp.align(cloudMap);
+    if(icp.hasConverged())
+    {
+        ROS_WARN("ICP has converged");
+        icp.getFitnessScore();
+        icp.getFinalTransformation();
+        own_globalMap_pcd = cloudMap;
+    }
+    else
+    {
+        ROS_WARN("ICP has not converged");
+    }
+    ROS_WARN("ICP done");
+    //convert to ROS message
+    pcl::toROSMsg(own_globalMap_pcd, globalMap_pcd);
+    globalMap_pcd.header.frame_id = "map";
 
 }
 
