540b801186c9df1eccf7376d6dd245b00c87530d
Vinh Trung <trung_thai@live.dk>
2022-04-25 10:40:01 +0200

    trying without header

--- catkin_ws/src/autoSSAR/src/mapmerge_node.cpp
+++ catkin_ws/src/autoSSAR/src/mapmerge_node.cpp
@@ -1,5 +1,3 @@
-
-#include "mapMerge/mapmerge.h"
 #include "ros/ros.h"
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -44,6 +42,38 @@ void getLocalMapCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
     pcl::fromROSMsg(localMap_pcd, cloudMap);
     local_map_pcd += cloudMap;
     own_globalMap_pcd += cloudMap;
+}
+
+void mergeMaps(pcl::PointCloud<pcl::PointXYZ>& map_in, sensor_msgs::PointCloud2 map_out)
+{
+    ROS_WARN("Merging maps");
+    
+    pcl::PointCloud<pcl::PointXYZ>::Ptr map_in_ptr(new pcl::PointCloud<pcl::PointXYZ>(map_in));
+    pcl::PointCloud<pcl::PointXYZ>::Ptr map_out_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr map_out_ptr_tmp(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg(map_out, *map_out_ptr_tmp);
+    //Remove NAN points
+    std::vector<int> indices;
+    // pcl::removeNaNFromPointCloud(*map_in_ptr, *map_in_ptr, indices);
+
+    pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+    icp.setInputSource(map_in_ptr);
+    icp.setInputTarget(map_out_ptr);
+    icp.align(*map_out_ptr_tmp);
+    if(icp.hasConverged())
+    {
+        ROS_WARN("ICP has converged");
+        icp.getFitnessScore();
+        icp.getFinalTransformation();
+        pcl::transformPointCloud(*map_in_ptr, *map_out_ptr_tmp, icp.getFinalTransformation());
+        pcl::concatenateFields(*map_out_ptr_tmp, *map_out_ptr, *map_out_ptr);
+        pcl::toROSMsg(*map_out_ptr, map_out);
+    }
+    else
+    {
+        ROS_WARN("ICP has not converged");
+    }
+    ROS_WARN("ICP done");
 }
 
 
