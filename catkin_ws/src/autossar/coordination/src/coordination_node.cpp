c72de564ca35b4db4f35a18332d2ffe7237bb99e
malle <malle.e.j@hotmail.com>
2022-04-29 14:47:07 +0200

    UAV died fixed, init pos fixed --- MID COMMIT TO TEST TRUNGS

--- catkin_ws/src/autossar/coordination/src/coordination_node.cpp
+++ catkin_ws/src/autossar/coordination/src/coordination_node.cpp
@@ -1,26 +1,33 @@
-#include "ros/ros.h"
+#include <ros/ros.h>
+#include "coordination/coordination_algorithm.h"
+
+
 
 
 int main(int argc, char **argv)
 {
-  ROS_INFO("within_range_node started");
-  ros::init(argc, argv, "within_range_node");
+  ROS_INFO("coordination_node started");
+  ros::init(argc, argv, "coordination_node");
   ros::NodeHandle nh;
 
-  
-  // Set rate
-  ros::Rate r(10); // 10 hz
-
-  while (ros::ok()) {
-
-    ros::spinOnce();
-    r.sleep();
-  }
+  coordinationAlgorithm coordAlgo;
+  coordAlgo.init(nh);
 
 
+  // // Set rate
+  // ros::Rate r(10); // 10 hz
+
+  // while (ros::ok()) {
+
+  //   ros::spinOnce();
+  //   r.sleep();
+  // }
 
 
-  std::cout << "Closing within_range_node" << std::endl;
+
+
+  ros::Duration(1.0).sleep();
   ros::spin();
+  std::cout << "Closing coordination_node" << std::endl;
   return 0;
 }
