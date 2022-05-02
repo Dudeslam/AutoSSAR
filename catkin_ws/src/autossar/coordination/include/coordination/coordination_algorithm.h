6654af281557358650e8590f7f0ec1c1f8a275e6
malle <malle.e.j@hotmail.com>
2022-05-02 19:35:52 +0200

    todays progress in coordination class

--- catkin_ws/src/autossar/coordination/include/coordination/coordination_algorithm.h
+++ catkin_ws/src/autossar/coordination/include/coordination/coordination_algorithm.h
@@ -2,15 +2,29 @@
 #define _COORDINATION_ALGORITHM_
 
 #include <ros/ros.h>
+#include "nav_msgs/Odometry.h"
 #include <nav_msgs/Path.h>
 #include <std_msgs/Empty.h>
+#include "std_msgs/String.h"
+#include <iostream>
 #include <string>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 
 
+enum odomPos { HOME=0, RELAY_POINT };
+enum COORD_STATE { EXPLORE, MEET, SACRIFICE, RELAY, FINISH, DEAD};
+std::vector<std::string> state_str_ = { "EXPLORE", "MEET", "SACRIFICE", "RELAY", "FINISH", "DEAD" };
 
-enum COORD_STATE { EXPLORE, MEET, SACRIFICE, RELAY, FINISH };
+struct otherUAVdata {
+    std::string name;
+    nav_msgs::Odometry relayPoint;
+    //std::vector<nav_msgs::Odometry> odom_record;    // array for all positions
+    int id;
+    bool inRange;
+};
+
+
 
 class coordinationAlgorithm {
 private:
@@ -21,28 +35,35 @@ private:
     pcl::PointCloud<pcl::PointXYZ> sharedPointCloud_;
     
     // Internal state var
-    COORD_STATE state_;
     std::string selfUAV_;
-
 
     // ROS utils
     //ros::Timer exec_timer_, safety_timer_, vis_timer_, frontier_timer_;
-    ros::Subscriber within_range_, battery_;
+    ros::Subscriber within_range_sub_, battery_sub_, odom_sub_;
+
+
+    COORD_STATE state_;
     double batteryCapasity_;
+    double distTraversed_;
     double rangeLeft_;
-    bool otherUAV0InRange_;
-    bool otherUAV1InRange_;
-    //ros::Publisher replan_pub_, new_pub_, bspline_pub_;
+    bool relayFlag_;
+    otherUAVdata nearUAV_;                          // UAV which is within range
+    std::vector<otherUAVdata> UAVsEncountered_;     // array for all UAVs encountered
+    nav_msgs::Odometry homeOdom_;
+    nav_msgs::Odometry currentOdom_;
+
+    ros::Timer meet_timer_;                         // Timer for triggering MEET
 
     // Util callbacks
     //void timerCallback(const ros::TimerEvent& e);
-    void batteryCallback(const nav_msgs::PathConstPtr& msg);
-    void withinRangeCallback(const nav_msgs::PathConstPtr& msg);
+    void batteryCallback(const std_msgs::String& msg);
+    void withinRangeCallback(const std_msgs::String& msg);
+    void odometryCallback(const nav_msgs::Odometry::ConstPtr& msg);
 
-    // helper functions
-    //int callExplorationPlanner();
+    // helper functions     (new state, whoCalled)
+    void triggerMeet(const ros::TimerEvent& e)
     void transitState(COORD_STATE new_state, std::string pos_call);
-
+    void updateRole(void);
 
 public:
     coordinationAlgorithm(/* args */) {
--- catkin_ws/src/autossar/coordination/src/coordination_algorithm.cpp
+++ catkin_ws/src/autossar/coordination/src/coordination_algorithm.cpp
@@ -1,7 +1,41 @@
 #include "coordination/coordination_algorithm.h"
+#include <typeinfo> // std::cout << " type is: " << typeid(VAR).name() << sdt::endl;
 
+int firstOdom = true;
+int getUavNr(std::string uavName){
+  int n = uavName.length();
+  char str[n+1];
+  strcpy(str, uavName.c_str());
+  
+  int tot, i, j=0, num[100];
+  tot = strlen(str);
+  for(i=0; i<tot; i++)
+  {
+    if(str[i]>='0' && str[i]<='9')
+    {
+        num[j] = str[i];
+        num[j] = num[j] - 48;
+        j++;
+    }
+  }
+  return num[0];
+}
 
-
+void updateNearUAV(void){
+  // Iterate all encountered
+  for(int uav_i : UAVsEncountered_){
+    // Add UAV if not already encountered
+    if(uav_i.name == nearUAV_.name){
+      // If relayFlag_
+      if(relayFlag_){
+        break;
+      }
+      uav_i.relayPoint = currentOdom_
+      break;
+    }
+    UAVsEncountered_.push_back(nearUAV_);
+  }
+}
 
 
 void coordinationAlgorithm::init(ros::NodeHandle& nh) {
@@ -12,41 +46,109 @@ void coordinationAlgorithm::init(ros::NodeHandle& nh) {
     // planner_manager_ = expl_manager_->planner_manager_;
 
     // Init state
-    state_ = COORD_STATE::EXPLORE;
     selfUAV_ = nh.getNamespace().c_str();
+    nh.getParam(selfUAV_+"/coordination/maxBattery", batteryCapasity_);
 
-    within_range_ = nh.subscribe(selfUAV_+"/within_range", 1, &coordinationAlgorithm::withinRangeCallback, this);
-    battery_      = nh.subscribe(selfUAV_+"/dist_traversed", 1, &coordinationAlgorithm::batteryCallback, this);
+    within_range_sub_ = nh.subscribe(selfUAV_+"/within_range", 1, &coordinationAlgorithm::withinRangeCallback, this);
+    battery_sub_      = nh.subscribe(selfUAV_+"/dist_traversed", 1, &coordinationAlgorithm::batteryCallback, this);
+    odom_sub_         = nh.subscribe(selfUAV_+"/state_ukf/odom", 1, &coordinationAlgorithm::odometryCallback, this);
     
-    otherUAV0InRange_ = false;
-    otherUAV1InRange_ = false;
+    state_ = COORD_STATE::EXPLORE;
+
+    distTraversed_ = 0;
+    rangeLeft_ = 0;
+    relayFlag_ = false;
+
+    // Timer for triggering MEET              2min
+    meet_timer_ = nh.createTimer(ros::Duration(120), &coordinationAlgorithm::triggerMeet, this);
+
 }
 
 
 
 
 void coordinationAlgorithm::batteryCallback(const std_msgs::String& msg){
-  double distTraversed = std::atof(msg->data)
-  rangeLeft_ = batteryCapasity_ - distTraversed;
-}
+  distTraversed_ = std::stod(msg.data);
+  rangeLeft_ = batteryCapasity_ - distTraversed_;
 
+  // Raise relayFlag_ if 50% distance left
+  if(rangeLeft_ < (batteryCapasity_/2) ){
+    relayFlag_ = true;
+  }
+
+  // std::cout << selfUAV_ << std::endl;
+  // std::cout << "rangeLeft_: " << rangeLeft_ << std::endl;
+}
 
 void coordinationAlgorithm::withinRangeCallback(const std_msgs::String& msg){
-  std::string str = msg.data;
 
-    if( str == otherUAV0 ){
-        otherUAV0InRange_ = true;
-    }
+  // Get info of who is near
+  nearUAV_.name = msg.data;
+  nearUAV_.inRange = true;
+  nearUAV_.id = getUavNr(msg.data);
 
-    if( str == otherUAV1 ){
-        otherUAV1InRange_ = true;
-    }
+  transitState(MEET, "withinRangeCallback");
+  
+  // std::cout << selfUAV_ << std::endl;
+  // std::cout << "otherUAVInRange_: " << otherUAV << std::endl;
+}
+
+void coordinationAlgorithm::odometryCallback(const nav_msgs::Odometry::ConstPtr& msg){
+  // First call = set Home-position = odom_record[0]
+  if( firstOdom ){
+    homeOdom_ = (*msg);
+    firstOdom = false;
+  }
+  // Update odom
+  currentOdom_ = (*msg);
+  
+  
+  // std::cout << selfUAV_ << std::endl;
+  // std::cout << "homeOdom_    Position-> x: ["<< homeOdom_.pose.pose.position.x<<"], \ty: ["<<homeOdom_.pose.pose.position.y<<"], \tz: ["<<homeOdom_.pose.pose.position.z  <<"]" << std::endl;
+  // std::cout << "currentOdom_ Position-> x: ["<< currentOdom_.pose.pose.position.x<<"], \ty: ["<<currentOdom_.pose.pose.position.y<<"], \tz: ["<<currentOdom_.pose.pose.position.z  <<"]" << std::endl;
+}
+
+void coordinationAlgorithm::triggerMeet(const ros::TimerEvent& e){
+  transitState(MEET, "triggerMeet");
+}
+
+void coordinationAlgorithm::transitState(COORD_STATE new_state, std::string pos_call) {
+  int pre_s = int(state_);
+  state_ = new_state;
+  cout << "[" + pos_call + "]: from " + fd_->state_str_[pre_s] + " to " + fd_->state_str_[int(new_state)] << endl;
 }
 
 
+void coordinationAlgorithm::updateRole(void){
 
-void coordinationAlgorithm::transitState(COORD_STATE new_state, std::string pos_call) {
-  // int pre_s = int(state_);
-  // state_ = new_state;
-  // cout << "[" + pos_call + "]: from " + fd_->state_str_[pre_s] + " to " + fd_->state_str_[int(new_state)] << endl;
+
+  switch (state_) {
+    case EXPLORE: {
+      // Nothing to do
+    }
+
+    case MEET: {
+      updateNearUAV();  // Adds UAV if new + update odom
+      // Nothing to do
+    }
+
+    case SACRIFICE: {
+      // Nothing to do
+    }
+
+    case RELAY: {
+      // Nothing to do
+    }
+
+    case DEAD: {
+      // Nothing to do
+    }
+
+    case default:{
+      ROS_WARN("DEFAULT REACHED =================================");
+    }
+  }
+
+
+  
 }
