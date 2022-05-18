def1ec2a47e64fab0a098ae247f2f85a3bb9400f
Vinh Trung <trung_thai@live.dk>
2022-05-18 11:24:25 +0200

    no message

--- catkin_ws/src/autoSSAR/mapMerge/src/coverage.cpp
+++ catkin_ws/src/autoSSAR/mapMerge/src/coverage.cpp
@@ -9,7 +9,7 @@ void coverage::init(ros::NodeHandle& nh) {
     std::cout << "*************************************************************" << std::endl;
 
     GlobalmapSize_sub_ = nh.subscribe("/map_generator/global_cloud", 1, &coverage::mapSize_callback, this);
-    selfUAVMapSize_sub_ = nh.subscribe(selfUAV + "/MergedMap", 1, &coverage::mergedMapSize_callback, this);
+    selfUAVMapSize_sub_ = nh.subscribe(selfUAV + "/sdf_map/occupancy_all", 1, &coverage::mergedMapSize_callback, this);
     
     coverage_pub_ = nh.advertise<std_msgs::String>(selfUAV+"/map_generator/coverage", 1);
 
@@ -32,12 +32,12 @@ void coverage::mergedMapSize_callback(const sensor_msgs::PointCloud2::ConstPtr& 
 
 void coverage::mapCoveredCallback(const ros::TimerEvent& event) {
     if(Globalmap_size != 0 && SelfMapSize != 0) {
-        auto cover = static_cast<double>(Globalmap_size / SelfMapSize)*100;
+        auto cover = static_cast<double> (((Globalmap_size-SelfMapSize)/Globalmap_size));
         std_msgs::String msg;
         msg.data = std::to_string(cover);
         coverage_pub_.publish(msg);
         std::cout << "Global map size: " << Globalmap_size << std::endl;
-        std::cout << "Self map size: " << SelfMapSize << std::endl;
+        std::cout << selfUAV <<" map size: " << SelfMapSize << std::endl;
         std::cout << "Total cover" << cover << "%" << std::endl;
     }
 
