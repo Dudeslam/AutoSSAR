274b7faa34bc5f3df157707b45691a2854d5957a
Vinh Trung <trung_thai@live.dk>
2022-05-18 11:07:10 +0200

    Types are stupid sometimes

--- catkin_ws/src/autoSSAR/mapMerge/src/coverage.cpp
+++ catkin_ws/src/autoSSAR/mapMerge/src/coverage.cpp
@@ -8,7 +8,7 @@ void coverage::init(ros::NodeHandle& nh) {
     std::cout << "Self: "<< selfUAV << std::endl;
     std::cout << "*************************************************************" << std::endl;
 
-    Globalmap_sub_ = nh.subscribe("/map_generator/global_cloud", 1, &coverage::mapSize_callback, this);
+    GlobalmapSize_sub_ = nh.subscribe("/map_generator/global_cloud", 1, &coverage::mapSize_callback, this);
     selfUAVMapSize_sub_ = nh.subscribe(selfUAV + "/MergedMap", 1, &coverage::mergedMapSize_callback, this);
     
     coverage_pub_ = nh.advertise<std_msgs::String>(selfUAV+"/map_generator/coverage", 1);
@@ -30,7 +30,7 @@ void coverage::mergedMapSize_callback(const sensor_msgs::PointCloud2::ConstPtr& 
     std::cout << selfUAV << " Map Size: " << SelfMapSize << std::endl;
 }
 
-void coverage::mapCoveredCallback() {
+void coverage::mapCoveredCallback(const ros::TimerEvent& event) {
     if(Globalmap_size != 0 && SelfMapSize != 0) {
         auto cover = (Globalmap_size / SelfMapSize)*100;
         std_msgs::String msg;
